3f7e81e1b403ba54a360d336f59c580ea465f5c1
Victoria Blackburn <victoriablackburn@Victorias-Air.lan>
2020-10-30 13:38:13 -0700

    updated server

--- Develop/public/db.js
+++ Develop/public/db.js
@@ -27,5 +27,33 @@ function saveRecord(record){
 }
 
 function checkDatabase(){
-    
+
+    const workout= db.workout(["add exercise"], "readwrite");
+
+    const store= workout.objectStore("add exercise");
+
+    const getAll= store.getAll();
+
+    getAll.onsuccess= function(){
+        if(getAll.result.length>0) {
+            fetch("/api/workouts", {
+            method: "POST",
+            body: JSON.stringify(getAll.result),
+            headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+            }    
+            })
+            .then(response => response.json())
+            .then(()=> {
+                const workout= db.workout(["add exercise"], "readwrite");
+
+                const store= workout.objectStore("add exercise"); 
+
+                store.clear();
+            });
+        }
+    };
 }
+
+window.addEventListener("online", checkDatabase);
